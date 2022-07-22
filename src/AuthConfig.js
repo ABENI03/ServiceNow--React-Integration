cd036beabf72cc70c2fcf73177d89fb1d9245257
Abenezer Anbessie <52824375+ABENI03@users.noreply.github.com>
2022-07-22 12:21:32 +0300

    Add files via upload

---
+++ src/AuthConfig.js
@@ -0,0 +1,9 @@
+const username = 'admin';
+const password = "Y+HZ6ao9fM!h";
+const url ='https://dev126634.service-now.com/api/now/table'
+
+module.exports ={
+    username,
+    password,
+    url
+}
